58160b40f26d20641092ae0cf883a4248a64a7d4
mackeynichols <mackey.nichols@gmail.com>
2018-11-25 22:00:26 -0500

    fixed dependencies, changed data sources

--- base.py
+++ base.py
@@ -1,21 +1,33 @@
 #! python3
-# base.py - go to envt canada site, compare today's temp to today's avg historical temp
+# base.py - go to envt canada site, compare yest's temp to yest's avg historical temp
 
-import requests, bs4, tweepy, os, time
+import requests, bs4, os, datetime, tweepy
 
 def check_temps():
-	# returns true if today's forecast beats today's average high, as per weather.gc.ca's website, in Toronto
-	soup = bs4.BeautifulSoup(requests.get('https://weather.gc.ca/city/pages/on-143_metric_e.html').text, "html.parser")
-	today = int(soup.select('span.high.wxo-metric-hide')[0].contents[0][:-2])
-	average = int(soup.select('td > span.wxo-metric-hide')[0].contents[0][:-2])
-	print("Today's forecast (C):")
-	print(today)
+	# returns true if yest's forecast beats yest's average high, as per weather.gc.ca's website, in Toronto
+	yest_date = datetime.datetime.now() - datetime.timedelta(1)
 
-	print("Today's average high (C):")
-	print(average)
+	yest_day = yest_date.day
+	yest_month = yest_date.month
+	yest_year = yest_date.year
+	yest_url = "http://climate.weather.gc.ca/climate_data/daily_data_e.html?Year="+str(yest_year)+"&Month="+str(yest_month)+"&Day="+str(yest_day)+"&StationID=51058&Prov=NT&urlExtension=_e.html"
+	historic_url = "http://climate.weather.gc.ca/climate_normals/results_1981_2010_e.html?stnID=1706&autofwd=1"
+	print(yest_url)
+
+	yest_date_str = yest_date.strftime('%B %d, %Y')
+	print(yest_date_str)
+
+	yest_soup = bs4.BeautifulSoup(requests.get(yest_url).text, "html.parser")
+	yest_temp = yest_soup.select('div#dynamicDataTable > table > tbody > tr')[yest_day].select('td')[1].text
+	print("Yesterday's High:\n"+yest_temp)
+
+	historic_soup = bs4.BeautifulSoup(requests.get(historic_url).text, "html.parser")
+	historic_temp = historic_soup.select('div.table-responsive > table > tbody > tr')[2].select('td')[yest_month-1].text
+	print("Yesterday's Historic Monthly high:\n" + historic_temp)
+
 	
-	print("Is today's temp above the daily historic average?")
-	print(today > average)
+	print("Is yesterday's temp above the daily historic average?")
+	print(float(yest_temp) > float(historic_temp))
 
 
 def tweet():
@@ -30,8 +42,8 @@ def tweet():
 	api = tweepy.API(auth)
 
 	user = api.me()
-	api.update_status('Testing Testing ' + str(time.time()) )
+	api.update_status('Testing Testing') 
 
 
 check_temps()
-tweet()
+#tweet()
